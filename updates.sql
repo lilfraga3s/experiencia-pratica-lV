8531157a2260b7ce455b0ed0951bcb9cb20db18d
lilfraga3s <gabriefragasilva23@gmail.com>
2025-11-18 13:16:34 -0300

    Update updates.sql

--- updates.sql
+++ updates.sql
@@ -1,1 +1,16 @@
--- updates SQL content here
+USE fastcar;
+
+-- 1. Atualizar o status de um veículo
+UPDATE veiculo
+SET status = 'manutencao'
+WHERE id_veiculo = 2;
+
+-- 2. Corrigir email de cliente
+UPDATE cliente
+SET email = 'ana.silva@novoemail.com'
+WHERE id_cliente = 1;
+
+-- 3. Atualizar valor total da devolução
+UPDATE devolucao
+SET valor_total = 520.00
+WHERE id_devolucao = 1;
