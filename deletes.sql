e47c3ad6ba9b97f9b7160a4e81790303e0edb98d
lilfraga3s <gabriefragasilva23@gmail.com>
2025-11-18 13:17:05 -0300

    Update deletes.sql

--- deletes.sql
+++ deletes.sql
@@ -1,1 +1,13 @@
--- deletes SQL content here
+USE fastcar;
+
+-- 1. Remover multa específica
+DELETE FROM multa_avaria
+WHERE id_multa = 1;
+
+-- 2. Remover uma manutenção registrada incorretamente
+DELETE FROM manutencao
+WHERE id_manutencao = 1;
+
+-- 3. Deletar reserva não vinculada a locação
+DELETE FROM reserva
+WHERE id_reserva = 2 AND id_reserva NOT IN (SELECT id_reserva FROM locacao);
